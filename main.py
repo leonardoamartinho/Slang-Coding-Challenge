cb0b52854ea7c2118fa603b2c555d8f8fe96580a
Leonardo Alves Martinho <leonardomartinho15@gmail.com>
2022-11-28 20:25:07 -0300

    Trying to create list

--- main.py
+++ main.py
@@ -12,5 +12,17 @@ if(activities_response.status_code == 200):
     act_res_json = json.loads(activities_response.text) #This will contain the list of "activities"
     print(act_res_json) #Check for sucess
 
-    grp_id = {}
+    grp_id = {} #This will contain and group users by their respective IDs
+
+    for act in act_res_json:
+        user_id = act_res_json[act]
+
+        if user_id not in grp_id:
+            grp_id[user_id] = [] #Creates a new entry-list for this user id
+            print('New-List') #Check for sucess
+        
+        grp_id[user_id].append(act)
+
+    print(grp_id) #Check for sucess
+
 
