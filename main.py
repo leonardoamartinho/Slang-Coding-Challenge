dd9d79fdd1c4dd071858315951a624d3e710659b
Leonardo Alves Martinho <leonardomartinho15@gmail.com>
2022-11-28 20:32:22 -0300

    Managed to create the list. Yay!

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ if(activities_response.status_code == 200):
     act_res_json = json.loads(activities_response.text) #This will contain the list of "activities"
     print(act_res_json) #Check for sucess
 
-    grp_id = {} #This will contain and group users by their respective IDs
+    grp_id = {} #This dictionary will contain and group users by their respective IDs
 
-    for act in act_res_json:
-        user_id = act_res_json[act]
+    for activities in act_res_json['activities']:
+        user_id = activities['user_id']
 
         if user_id not in grp_id:
             grp_id[user_id] = [] #Creates a new entry-list for this user id
             print('New-List') #Check for sucess
         
-        grp_id[user_id].append(act)
+        grp_id[user_id].append(activities)
 
     print(grp_id) #Check for sucess
+
+    for user_id in grp_id:
+        act_id = [] #Creates list for activities IDs
+        for activities in grp_id[user_id]:
+            act_id.append(activities['id']) #Appends the activities to the list
+        grp_id[user_id] = act_id
 
 
