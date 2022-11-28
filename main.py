f66afe22e1a471ec3395ec8de033db76535003d7
Leonardo Alves Martinho <leonardomartinho15@gmail.com>
2022-11-28 20:45:30 -0300

    Grouping activities by ID

--- main.py
+++ main.py
@@ -14,21 +14,34 @@ if(activities_response.status_code == 200):
 
     grp_id = {} #This dictionary will contain and group users by their respective IDs
 
-    for activities in act_res_json['activities']:
-        user_id = activities['user_id']
+    for act in act_res_json['activities']:
+        user_id = act['user_id']
 
         if user_id not in grp_id:
             grp_id[user_id] = [] #Creates a new entry-list for this user id
             print('New-List') #Check for sucess
         
-        grp_id[user_id].append(activities)
+        grp_id[user_id].append(act)
 
     print(grp_id) #Check for sucess
 
     for user_id in grp_id:
         act_id = [] #Creates list for activities IDs
-        for activities in grp_id[user_id]:
-            act_id.append(activities['id']) #Appends the activities to the list
+        for act in grp_id[user_id]:
+            act_id.append(act['id']) #Appends the activities to the list
         grp_id[user_id] = act_id
+
+    grpid_actid = {}
+
+    for user_id in grp_id:
+        grpid_actid[user_id] = {}
+
+        for act_id in grp_id[user_id]:
+            grpid_actid[user_id][act_id] = {}
+
+            for activities in act_res_json['id']:
+
+                if(activities['id'] == act_id):
+                    grpid_actid[user_id][act_id] = activities
 
 
