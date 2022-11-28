5f6aef99a47ede167863d8b543974f9404996b9d
Leonardo Alves Martinho <leonardomartinho15@gmail.com>
2022-11-28 20:13:01 -0300

    Retrieved Endpoint

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import requests
 import json
 
 url_get = "https://api.slangapp.com/challenges/v1/activities"
-
 header = {'Content-Type': 'application/json', 'Authorization': 'Basic MTQxOjVGZHZTamV1V01MQ0tZcFRySk9TemQ1ZHVKbjdhVm9salNTZmlGUXp6Qk09'}
 
 activities_response = requests.get(url_get, headers = header)
+print(activities_response.url) #confirms url get
+
+if(activities_response.status_code == 200):
+    print('Yes!') #Check for sucess in retrieving the endpoint
+
+
